3f6a4226ce8eda596a44fee451a4fb4d3517e37e
Jejomar Parrilla <jpar1252003@gmail.com>
2025-09-05 23:11:18 +0800

    Backend: fix(favorites): Cleanup the Model & API

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterUserController;
+use App\Http\Controllers\Products\FavoriteController;
 use App\Http\Controllers\Products\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,5 +28,11 @@ Route::middleware('auth:sanctum')->group(function () {
 
         // // Extra endpoint to sync from FakeStoreAPI
         // Route::post('/sync', [ProductController::class, 'syncProducts']);
+    });
+
+    Route::prefix('favorites')->group(function () {
+        Route::get('/', [FavoriteController::class, 'index']);      // List favorites (with pagination + search)
+        Route::post('/', [FavoriteController::class, 'store']);     // Add product to favorites
+        Route::delete('/{product}', [FavoriteController::class, 'destroy']); // Remove product from favorites
     });
 });
