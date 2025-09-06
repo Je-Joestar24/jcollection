323393d597431d310d6c106ea0c66a232e794aca
Jejomar Parrilla <jpar1252003@gmail.com>
2025-09-06 12:48:02 +0800

    Frontend: chore: Tailwind CSS integrated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,11 @@
 import { useState, useEffect } from 'react'
 import { Outlet, Link } from 'react-router-dom'
-import { fetchTest } from './services/test';
 
 function App() {
-  
-  const test = () => fetchTest()
-  
-  // ✅ onMounted (runs once when component mounts)
-  useEffect(() => {
-    console.log("Component mounted");
-    return () => {
-      console.log("Component unmounted");
-    };
-  }, []);
-  
   return (
     <>
       <div>
         <nav>
-          <button onClick={test}>test</button>
           <Link to="/">Home</Link> | <Link to="/about">About</Link>
         </nav>
         <main>
